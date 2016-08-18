2e66a109db4e02662f8ca2dcb2e0962761319680
Miguel Olmedo <molmedo@idealista.com>
2016-08-18 13:10:28 +0200

    Creamos view controller de favoritos

---
+++ Interurbanos/Interurbanos/FavoritesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FavoritesViewController.swift
+//  Interurbanos
+//
+//  Created by Miguel Olmedo on 18/08/16.
+//  Copyright © 2016 molmedo. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController
+{
+}
