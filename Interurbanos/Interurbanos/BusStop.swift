16ac38de6b495698a3a2d715421df2e5d0a64695
Miguel Olmedo <miguel.olmedo.camacho@gmail.com>
2016-09-08 23:39:52 +0200

    Creado tanto el objeto de dominio de la parada como el view model para el pintado

---
+++ Interurbanos/Interurbanos/BusStop.swift
@@ -0,0 +1,12 @@
+//
+//  BusStop.swift
+//  Interurbanos
+//
+//  Created by Miguel Olmedo on 08/09/2016.
+//  Copyright © 2016 molmedo. All rights reserved.
+//
+
+struct BusStop
+{
+    
+}
---
+++ Interurbanos/Interurbanos/BusStopVM.swift
@@ -0,0 +1,12 @@
+//
+//  BusStopVM.swift
+//  Interurbanos
+//
+//  Created by Miguel Olmedo on 08/09/2016.
+//  Copyright © 2016 molmedo. All rights reserved.
+//
+
+struct BusStopVM
+{
+    
+}
